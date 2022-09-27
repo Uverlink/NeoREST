4511909c3066e0a0fbf896c4f01a473603395539
Uverlink <marc.pizzanelli@gmail.com>
2022-09-27 09:37:15 +0200

    Queries TS

--- src/user/queries.js
+++
@@ -1,9 +0,0 @@
-const getUsers = "SELECT * FROM users";
-const getUserById = "SELECT * FROM users WHERE id = $1";
-const checkNicknameExists = "SELECT s FROM users s WHERE s.nickname = $1";
-const addUser = "INSERT INTO users (nickname, bio, password, fullname) VALUES ($1, $2, $3, $4)";
-const deleteUser = "DELETE FROM users WHERE id = $1";
-
-module.exports = {
-    getUsers, getUserById, checkNicknameExists, addUser, deleteUser,
-};
